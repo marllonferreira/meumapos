27b99567138bbd52ed10f5533b3206c5942aedfb
Marllon Ferreira <marllon-ferreira@hotmail.com>
2022-04-10 16:13:47 -0300

    mostra documento do cliente na impressão

--- application/views/os/imprimirOsTermica.php
+++ application/views/os/imprimirOsTermica.php
@@ -60,7 +60,8 @@ $totalProdutos = 0; ?>
                                                 <span>
                                                     <b>Cliente: </b>
                                                     <span><?php echo $result->nomeCliente ?></span>
-                                                    <span style="padding-left: 4%;"><b>Celular:</b> <?php echo $result->celular_cliente ?></span>
+                                                    <span>CPF/CNPJ: <?php echo $result->documento_cliente ?></span>
+                                                    <span style="padding-left: 1%;"><b>Celular:</b> <?php echo $result->celular_cliente ?></span>
                                             </li>
                                         </ul>
                                     </td>
