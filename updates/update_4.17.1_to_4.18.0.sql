fbb85faa1612ac54d815e72b597d8d7679ef446c
Marllon Ferreira <marllon-ferreira@hotmail.com>
2022-04-18 12:08:27 -0300

    d

--- updates/update_4.17.1_to_4.18.0.sql
+++
@@ -1,2 +0,0 @@
--- Alterando tabela vendas
-ALTER TABLE vendas ADD observacoes_cliente TEXT DEFAULT NULL NULL;
--- updates/update_4.19.0_to_4.20.0.sql
+++
@@ -1,2 +0,0 @@
--- Alterando tabela lancamentos
-ALTER TABLE lancamentos ADD observacoes TEXT DEFAULT NULL NULL;
