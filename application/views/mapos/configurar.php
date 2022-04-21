555423222bbdef399305e2bc5a95a1b031164e61
Marllon Ferreira <marllon-ferreira@hotmail.com>
2022-04-21 17:34:53 -0300

    desativado Atualizar Mapos
    desativado Atualizar Mapos temporariamente. ate a função volta a funcionar

--- application/views/mapos/configurar.php
+++ application/views/mapos/configurar.php
@@ -208,7 +208,7 @@
                                 <div class="span9" style="display:flex">
                                     <button href="#modal-confirmabanco" data-toggle="modal" type="button" class="button btn btn-warning">
                                       <span class="button__icon"><i class="bx bx-sync"></i></span><span class="button__text2">Banco de Dados</span></button>
-                                    <button href="#modal-confirmaratualiza" data-toggle="modal" type="button" class="button btn btn-danger">
+                                    <button href="#modal-confirmaratualiza" data-toggle="modal" type="button" class="button btn btn-danger" disabled>
                                       <span class="button__icon"><i class="bx bx-sync"></i></span><span class="button__text2">Atualizar Mapos</span></button>
                                 </div>
                             </div>
